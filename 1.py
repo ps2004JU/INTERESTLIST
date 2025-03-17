bcb0a03c7f046795cba0ef1c3ec789e2a140c729
ps2004JU <punya.22bcan062@jecrcu.edu.in>
2025-03-17 12:01:18 +0530

    Second comm on main

--- 1.py
+++ 1.py
@@ -1,16 +1,16 @@
 while True:
-    x = open("file1.txt","r")
+    x = open("MYF.txt","r")
     c = len(x.readlines())
     x.close()
     i=input("For adding content press a, for reading press r, and to exit press any button :: ")
     if(i=='a' or i=="A"):
-        a=open("file1.txt","a")
+        a=open("MYF.txt","a")
         i1=input("Add ur interests :: ")        
         a.write(f"Interest {c+1} :: {i1}")
         print("Success")
         a.close()
     elif(i=="r" or i=="R"):
-        a=open("file1.txt","r")
+        a=open("MYF.txt","r")
         print(a.read())
         a.close()
     else:
